b079a7c231fad5a7e6d757501490c120a0d69074
Stockholder <stockholder911@gmail.com>
2014-01-02 22:42:59 -0200

    Created links

--- app/views/tests/create.blade.php
+++ app/views/tests/create.blade.php
@@ -14,11 +14,13 @@
         <li>
             {{ Form::label('languages_id', 'Languages_id:') }}
             {{ Form::select('languages_id', $languages_options); }}
+            {{ link_to_route('languages.index', 'Add new', array(), array('class' => 'btn btn-info', 'target' => 'blank')) }}
         </li>
 
         <li>
             {{ Form::label('affiliates_id', 'Affiliates_id:') }}
             {{ Form::select('affiliates_id', $affiliates_options); }}
+            {{ link_to_route('affiliates.index', 'Add new', array(), array('class' => 'btn btn-info', 'target' => 'blank')) }}
         </li>
 
         <li>
